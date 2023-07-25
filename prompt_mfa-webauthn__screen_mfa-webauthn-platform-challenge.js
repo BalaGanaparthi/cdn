c9e202b5b950150448610d63e86db8c1c9770ad3
Bala Ganaparthi <bala.ganaparthi@okta.com>
2023-07-24 22:38:17 -0500

    Add cvs js files

--- prompt_mfa-webauthn__screen_mfa-webauthn-platform-challenge.js
+++ prompt_mfa-webauthn__screen_mfa-webauthn-platform-challenge.js
@@ -3,6 +3,6 @@ var pwd = getCookie(COOKIE_NAME__PASSWORD);
 
 if(pwd != null && pwd != ""){
   var userPasswordButtonElement = getElementByXpath(XPATH_MFA_WEBAUTHN_PLATFORM_CHALLENGE__USE_PASSWORD_BUTTON);
-  userPasswordButtonElement.click()
+  // userPasswordButtonElement.click()
 }
  
