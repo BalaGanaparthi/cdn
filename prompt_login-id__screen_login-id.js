8498581ec8afedc0c732c458113bc87f32970206
Bala Ganaparthi <bala.ganaparthi@okta.com>
2023-09-01 20:45:06 -0500

    Fix : Modify prompt:login-id and only have sms changes (remove country code action)

--- prompt_login-id__screen_login-id.js
+++ prompt_login-id__screen_login-id.js
@@ -8,20 +8,20 @@ if(pwdlessCountryCodeButton != null){
     pwdlessCountryCodeButton.parentElement.parentElement.appendChild(pwdlessCountryCodeButton)
 }
 
-if (jsAvailable) {
-    populateUserFromCookie(COOKIE_NAME__LOGGED_IN_ACCT, XPATH_LOGIN_ID__USERNAME)
+// if (jsAvailable) {
+//     populateUserFromCookie(COOKIE_NAME__LOGGED_IN_ACCT, XPATH_LOGIN_ID__USERNAME)
 
-    var anchorElement = getElementByXpath(XPATH_LOGIN_ID__CONTINUE_BUTTON);
-    addPasswordInput(anchorElement);
-    addRememberMeCheckbox(anchorElement);
-    overrideButtonClicked(anchorElement);
+//     var anchorElement = getElementByXpath(XPATH_LOGIN_ID__CONTINUE_BUTTON);
+//     addPasswordInput(anchorElement);
+//     addRememberMeCheckbox(anchorElement);
+//     overrideButtonClicked(anchorElement);
 
-    //https://webauthn.me/browser-support
-    var webAuthNAvailableElement = getElementByXpath(XPATH_LOGIN_ID__WEBAUTHN_AVAILABLE);
-    var webAuthNPlatformAvailableElement = getElementByXpath(XPATH_LOGIN_ID__WEBAUTHN_PLATFORM_AVAILABLE);
-    const webAuthNAvailable = webAuthNAvailableElement.getAttribute("value"); // Hardware Authenticators / Roaming Biometrics.. like yubikeys
-    const webAuthNPlatformAvailable = webAuthNPlatformAvailableElement.getAttribute("value"); // Platform Biometrics.. like macOS Touch ID
-    if (webAuthNAvailable || webAuthNPlatformAvailable) { //Display only when webauthN (hardware || platform) is supported  
-        addLoginWithBiometricsLink();
-    }
-}
+//     //https://webauthn.me/browser-support
+//     var webAuthNAvailableElement = getElementByXpath(XPATH_LOGIN_ID__WEBAUTHN_AVAILABLE);
+//     var webAuthNPlatformAvailableElement = getElementByXpath(XPATH_LOGIN_ID__WEBAUTHN_PLATFORM_AVAILABLE);
+//     const webAuthNAvailable = webAuthNAvailableElement.getAttribute("value"); // Hardware Authenticators / Roaming Biometrics.. like yubikeys
+//     const webAuthNPlatformAvailable = webAuthNPlatformAvailableElement.getAttribute("value"); // Platform Biometrics.. like macOS Touch ID
+//     if (webAuthNAvailable || webAuthNPlatformAvailable) { //Display only when webauthN (hardware || platform) is supported  
+//         addLoginWithBiometricsLink();
+//     }
+// }
